01708fa76800c314ba40177c5569567af3e1e769
mohanraj kanthasamy <mohanrajkanthasamy@mohanrajs-MacBook-Pro.local>
2019-03-18 22:40:05 +0530

    find by params id

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
-  before_action :find_todo, except: %i[create index]
+  # before_action :find_todo, except: %i[create index]
 
   # GET /todos
   def index
@@ -9,7 +9,7 @@ class TodosController < ApplicationController
 
   # GET /todos/:id
   def show
-    # @todo = Todo.find_by_id!(params[:_id])
+    @todo = Todo.find_by_id!(params[:_id])
     render json: @todo , status: :ok
   end
 
@@ -25,25 +25,30 @@ class TodosController < ApplicationController
 
   # PUT /todos/:id
   def update
-    # @todo = Todo.find_by_id!(params[:_id])
-    @todo.update(todo_params)
-    head :no_content
+    @todo = Todo.find_by_id!(params[:_id])
+    if @todo.update(todo_params)
+      render json: @todo, status: :updated
+    else
+      render json: { errors: @todo.errors.full_messages}, status: :unprocessable_entity
+    end
+    # head :no_content
   end
 
    # DELETE /todos/:id
   def destroy
-    # @todo = Todo.find_by_id!(params[:_id])
+    @todo = Todo.find_by_id!(params[:_id])
     @todo.destroy
+    render json: @todo, status: :ok
   end
 
 
 private
 
-  def find_todo
-    @user = Todo.find_by_id!(params[:_id])
-    rescue ActiveRecord::RecordNotFound
-      render json: { errors: 'Todo not found' }, status: :not_found
-  end
+  # def find_todo
+  #   @user = Todo.find_by_id!(params[:_id])
+  #   rescue ActiveRecord::RecordNotFound
+  #     render json: { errors: 'Todo not found' }, status: :not_found
+  # end
 
   def todo_params
     params.permit(:title, :description, :due_date, :status)
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   
   resources :todos, param: :_id
 
-  # scope 'api/v1' do
-  # 	resources :todos, param: :_id
-  # end
-
   get '/*a', to: 'application#not_found'
 
 end
